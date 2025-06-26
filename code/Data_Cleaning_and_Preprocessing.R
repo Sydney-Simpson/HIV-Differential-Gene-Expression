a950de4809b54a33e88b286a5707af22b243dd8d
Sydney-Simpson <sydney.r.simpson10@gmail.com>
2025-06-26 09:33:44 -0400

    Data_Cleaning_and_Preprocessing.R
    Retrieves desired datasets from GEO, cleans and preprocesses data to prepare for downstream analysis

---
+++ code/Data_Cleaning_and_Preprocessing.R
@@ -0,0 +1,141 @@
+
+# using GEOQuery download expression sets for relevant GEO series
+library(GEOquery)
+library(limma)
+library(umap)
+
+# list of GEO series IDs
+geo_series <- c("GSE195434", "GSE200606")
+
+# initialize lists for results
+raw_list <- c()
+sample_info_list <- c()
+exprs_list <- c()
+
+# loop through each series
+for (id in geo_series) {
+  # download the series
+  raw_data <- getGEO(id, GSEMatrix = TRUE, getGPL = FALSE)[[1]]
+
+  # store raw data
+  raw_list[[id]] <- raw_data
+  
+  # extract sample info
+  sample_info_list[[id]] <- pData(raw_data)
+  
+  # (Optional) extract and store expression matrix. However we want to do our own normalization 
+  exprs_list[[id]] <- exprs(raw_data)
+}
+
+
+# Map probes to gene symbols
+library(illuminaHumanv4.db)
+library(AnnotationDbi)
+
+#Make a new list for the annotated expression data
+Annotated <- c()
+  
+for (set in 1:length(exprs_list)) {
+  dataset <- data.frame(exprs_list[[set]])
+  probe_ids <- rownames(dataset)
+  dataset <- dataset %>%
+    mutate(symbol = mapIds(illuminaHumanv4.db,
+                       keys=probe_ids,
+                       column="SYMBOL",
+                       keytype="PROBEID",),
+           .before = colnames(dataset)[1])
+  #Remove NA
+  dataset <- na.omit(dataset)
+  #Add to the Annotated list
+  Annotated[[set]] <- dataset
+  # Ensure correct names
+  names(Annotated)[[set]] <- names(exprs_list)[[set]]
+}
+
+# Set up patient data
+library(tidyselect)
+library(stringr)
+
+p_data_1 <- sample_info_list[[1]]
+selected_info_1 <- p_data_1 %>%
+  dplyr::select(
+    geo_accession = "geo_accession",
+    patient_id = "individual:ch1",
+    status = "group:ch1",
+    time = "time:ch1"
+    ) %>%
+  mutate(time = str_split_i(time, " ", 1))
+
+# Substitute values on infection status so both data sets match
+selected_info_1$status <- gsub("Post.infection","HIV-1 infected", selected_info_1$status)
+selected_info_1$status <- gsub("Pre.infection","HIV negative", selected_info_1$status)
+
+
+p_data_2 <- sample_info_list[[2]]
+selected_info_2 <- p_data_2 %>%
+   dplyr::select(
+     geo_accession = "geo_accession",
+     status = "source_name_ch1",
+     treatment = "treatment:ch1",
+     gender = "gender:ch1",
+     age = "age:ch1"
+  )
+
+#save relevant sample information
+write.table (selected_info_1, "GSE195434_sample_info.txt", row.names= TRUE, quote= FALSE, sep="\t")
+write.table (selected_info_2, "GSE200606_sample_info.txt", row.names= TRUE, quote= FALSE, sep="\t")
+
+#save this information as an object
+sample_info <- c()
+sample_info[[1]] <- selected_info_1
+sample_info[[2]] <- selected_info_2
+
+names(sample_info) <- names(sample_info_list)
+
+
+
+# Combine expression data with patient info and batch process
+library(sva)
+# Make a new object to hold your processed data
+p_data <- bind_rows(sample_info) %>%
+  dplyr::select(geo_accession, status)
+
+# Combine dataset and adjust for batch effects
+d1 <- Annotated[[1]][,-1]
+d2 <- Annotated[[2]][,-1]
+
+data <- cbind(log2(d1), log2(d2))
+batch <- c(rep( 1, times = ncol(d1)), rep(2, times = ncol(d2)))
+mod <- model.matrix(~as.factor(status), data = p_data)
+final <- ComBat(data, mod = mod, batch = batch, par.prior = TRUE)
+
+# Restore gene symbol information
+symbols <- data.frame("GeneSymbol" = Annotated[[1]][,1])
+processed_data <- cbind(symbols, final)
+
+# Take median value for gene symbols with multiple probes
+processed_dataset <- processed_data %>%
+   group_by(GeneSymbol) %>%
+   summarize(across(everything(), \(x) median(x, na.rm = TRUE))) %>%
+   tibble::column_to_rownames(var = "GeneSymbol")
+
+#Save the final table
+write.table(processed_dataset, "Processed_data.txt", row.names = TRUE, quote = FALSE, sep = "\t")
+
+
+
+# Combine sample info
+s_info_1 <- selected_info_1 %>%
+  mutate(dataset = "GSE195434")
+
+# Make sure both sets have matching columns
+s_info_2 <- selected_info_2 %>%
+  dplyr::select(geo_accession , status) %>%
+  mutate(patient_id = c(1:nrow(selected_info_2)) ) %>%
+  mutate (time = NA) %>%
+  mutate(dataset = "GSE200606")
+
+all_sample_info <- rbind(s_info_1, s_info_2)
+
+#Save the final table
+write.table(all_sample_info, "all_sample_info.txt", row.names = TRUE, quote = FALSE, sep = "\t")
